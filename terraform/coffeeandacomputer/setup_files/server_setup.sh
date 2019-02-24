94a16e19f36a61e53beaa57d39ef0bd3c0875e83
noliverio <nick@nicholasoliverio.com>
2019-02-24 02:00:58 +0000

    Separating initialization script to bash file for increased manageability

---
+++ terraform/coffeeandacomputer/setup_files/server_setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+# This script is intended to be run as sudo on initialization of server.
+# Call with the following arguments: hostname, username, (interactive_server/puppet)
+
+function set_hostname {
+    chmod 666 /etc/sysconfig/network
+    echo 'HOSTNAME=$1.coffeeandacomputer.com' >> /etc/sysconfig/network
+    chmod 644 /etc/sysconfig/network
+    hostnamectl set-hostname secondary_dns.coffeeandacomputer.com
+}
+
+function interactive_server {
+    git clone https://github.com/noliverio/dotfiles.git /home/$2/.dotfiles
+    /home/$2/.dotfiles/setup.sh interactive_server
+}
+
+function puppet_managed {
+    rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+    yum install -y puppet-agent
+}
+
+function custom {
+    yum install -y bind bind-chroot bind-utils
+}
+
+
+
+
+yum update -y
+yum install -y git
+
+set_hostname $1
+
+if [ $3 == interactive_server ]
+then
+    interactive_server
+fi
+
+custom
+
