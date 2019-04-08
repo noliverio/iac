e796cc1c7a29a22799e06d1afca0ebdd4a8fe1c1
noliverio <nick@nicholasoliverio.com>
2019-04-07 20:19:43 -0400

    cleaned out old tutorial code, and moved some kubernetes cluster config into terraform

--- terraform/coffeeandacomputer/k8s_cluster.tf
+++ terraform/coffeeandacomputer/k8s_cluster.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "node" {
   ami           = "ami-02eac2c0129f6376b"
-  instance_type = "t3.micro"
+  instance_type = "t2.medium"
 
   vpc_security_group_ids = ["${module.base_network.webserver_sec_group}"]
 
@@ -35,7 +35,20 @@ resource "aws_instance" "node" {
     }
   }
 
-  count = 4
+  count = 6
+}
+
+resource "aws_security_group" "kubernetes_security_group" {
+  vpc_id = "${module.base_network.main_vpc}"
+}
+
+// For ease's sake allow all communication between k8s nodes.
+resource "aws_security_group_rule" "allow_kubernetes_internode_communication_tcp" {
+  type                     = "ingress"
+  source_security_group_id = "{$aws_security_group.kubernetes_security_group.id}"
+  from_port                = "0"
+  to_port                  = "65535"
+  protocol                 = "tcp"
 }
 
 output "node_public_ip${count.index}" {
--- terraform/module/README.md
+++
@@ -1,3 +0,0 @@
-Set up four new ec2 instances in two different subnets, with have an http load balancer pointing at each instance.
-
-Use bootstrap module to manage core terraform backend infrastructure.
